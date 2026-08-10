910960d2d2f7ac0bf12123a206db881679428a52
lhyzs-hub <3278287074@qq.com>
2026-08-11 00:37:22 +0800

    soften vinyl into grey tones

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "lhyzs-site-v12";
+const CACHE_VERSION = "lhyzs-site-v13";
 const CACHE_NAME = `lhyzs-offline-${CACHE_VERSION}`;
 const SCOPE_URL = new URL(self.registration.scope);
 const OFFLINE_PAGE = new URL("404.html", SCOPE_URL).href;
