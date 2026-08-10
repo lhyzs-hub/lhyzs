5d9c664f4d11f301249288156faf8c85a813ee35
lhyzs-hub <3278287074@qq.com>
2026-08-11 00:46:55 +0800

    correct vinyl tonearm placement

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "lhyzs-site-v14";
+const CACHE_VERSION = "lhyzs-site-v15";
 const CACHE_NAME = `lhyzs-offline-${CACHE_VERSION}`;
 const SCOPE_URL = new URL(self.registration.scope);
 const OFFLINE_PAGE = new URL("404.html", SCOPE_URL).href;
